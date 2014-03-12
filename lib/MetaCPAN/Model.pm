245b41837738dbd6af988c4d5e2652df72d6e62b
Olaf Alders <olaf@wundersolutions.com>
2014-03-12 14:54:54 -0400

    Format code in MetaCPAN::Model.

--- lib/MetaCPAN/Model.pm
+++ lib/MetaCPAN/Model.pm
@@ -1,28 +1,42 @@
 package MetaCPAN::Model;
+
+use strict;
+use warnings;
+
 use Moose;
 use ElasticSearchX::Model;
 
-analyzer lowercase => ( tokenizer => 'keyword',  filter   => 'lowercase' );
-analyzer fulltext  => ( type      => 'snowball', language => 'English' );
+analyzer lowercase => (
+    tokenizer => 'keyword',
+    filter    => 'lowercase',
+);
+
+analyzer fulltext => (
+    type     => 'snowball',
+    language => 'English',
+);
+
 tokenizer camelcase => (
     type => 'pattern',
     pattern =>
-        "([^\\p{L}\\d]+)|(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)|(?<=[\\p{L}&&[^\\p{Lu}]])(?=\\p{Lu})|(?<=\\p{Lu})(?=\\p{Lu}[\\p{L}&&[^\\p{Lu}]])"
+        "([^\\p{L}\\d]+)|(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)|(?<=[\\p{L}&&[^\\p{Lu}]])(?=\\p{Lu})|(?<=\\p{Lu})(?=\\p{Lu}[\\p{L}&&[^\\p{Lu}]])",
 );
+
 analyzer camelcase => (
     type      => 'custom',
     tokenizer => 'camelcase',
-    filter    => [ 'lowercase', 'unique' ]
+    filter    => [ 'lowercase', 'unique' ],
 );
 
 index cpan => (
     namespace => 'MetaCPAN::Document',
     alias_for => 'cpan_v1',
-    shards    => 5
+    shards    => 5,
 );
 
 index user => ( namespace => 'MetaCPAN::Model::User' );
 
 __PACKAGE__->meta->make_immutable;
+1;
 
 __END__
